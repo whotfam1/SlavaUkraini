ff86094856b1db84a9e8129ab3be8d070da03273
monkerzhe <100535731+monkerzhe@users.noreply.github.com>
2022-02-27 23:35:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ const server = http.createServer((req, res) => {
     res.end('Hello World');
 });
 
-server.listen(port, hostname, () => {
+server.listen(port,undefined,() => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
 
